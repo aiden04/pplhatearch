1726782ae97774428d9112fda4d9793eee60f28c
Aiden Tingler <tingting2565@gmail.com>
2025-11-22 19:17:54 -0500

    Update completion descriptions for pplhatearch

--- completions/pplhatearch.fish
+++ completions/pplhatearch.fish
@@ -6,7 +6,8 @@ complete -c pplhatearch -n "not __fish_seen_subcommand_from -S -R" -s S -d "Inst
 complete -c pplhatearch -n "not __fish_seen_subcommand_from -S -R" -s R -d "Remove packages"
 complete -c pplhatearch -o Ss -d "Search the backup AUR mirror" -r
 complete -c pplhatearch -o Si -d "Show package information" -r
-complete -c pplhatearch -s Q -d "Show information about installed packages"
+complete -c pplhatearch -s Q -d "List packages installed via pplhatearch"
+complete -c pplhatearch -l clear-cache -d "Remove cached package data and exit"
 complete -c pplhatearch -s C -d "Cache directory" -r -f -a "(__fish_complete_directories)"
 complete -c pplhatearch -l repo -d "Git mirror URL" -r
 complete -c pplhatearch -l color -d "Enable/disable colored output"
